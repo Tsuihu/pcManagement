4e4e67eac460deb282c21ca9faedaa94f25a272f
ningchen1 <3317559499@qq.com>
2022-11-08 17:19:38 +0800

    登录注册的MD5加密功能

--- src/main/java/com/higher/pcmanagement/controller/ManagerController.java
+++ src/main/java/com/higher/pcmanagement/controller/ManagerController.java
@@ -3,6 +3,7 @@ package com.higher.pcmanagement.controller;
 import com.higher.pcmanagement.pojo.Manager;
 import com.higher.pcmanagement.pojo.bo.LoginModel;
 import com.higher.pcmanagement.service.ManagerService;
+import com.higher.pcmanagement.util.MD5;
 import com.higher.pcmanagement.util.ResultCodeEnum;
 import com.higher.pcmanagement.util.ResultModel;
 import com.higher.pcmanagement.exception.BusinessException;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/manager")
@@ -26,7 +29,12 @@ public class ManagerController {
      */
     @PostMapping("login.do")
     public ResultModel<Manager> login(@RequestBody LoginModel model) throws BusinessException {
-        Manager manager = managerService.login(model.getTel(), model.getPassword());
+
+        //密码进行MD5加密后查询登录
+        String passwordMd5 = MD5.encrypt(model.getPassword());
+        System.out.println(passwordMd5);
+
+        Manager manager = managerService.login(model.getTel(),passwordMd5);
         //上面的方法只要不报错，就是调用成功，可以直接返回。
         return new ResultModel<>(ResultCodeEnum.SUCCESS, manager, "登录成功");
     }
@@ -38,6 +46,13 @@ public class ManagerController {
      */
     @PostMapping("registe.do")
     public ResultModel<Manager> registe(@RequestBody Manager manager) throws BusinessException {
+
+        //MD5加密
+        String password = manager.getPassword();
+        String signKeyMd5 = MD5.encrypt(password);
+        manager.setPassword(signKeyMd5);
+
+        manager.setRegistTime(new Date());
         managerService.addManager(manager);
         return new ResultModel<>(ResultCodeEnum.SUCCESS,manager,"注册成功");
     }
