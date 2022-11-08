b1f3e87018ab678564aa7125944ab38c9208be3c
ningchen1 <3317559499@qq.com>
2022-11-08 23:19:11 +0800

    修改了updateTestTube中的bug

--- src/main/java/com/higher/pcmanagement/service/impl/TestTubeServiceImpl.java
+++ src/main/java/com/higher/pcmanagement/service/impl/TestTubeServiceImpl.java
@@ -2,7 +2,6 @@ package com.higher.pcmanagement.service.impl;
 
 import com.higher.pcmanagement.dao.TestTubeDao;
 import com.higher.pcmanagement.exception.BusinessException;
-import com.higher.pcmanagement.pojo.Point;
 import com.higher.pcmanagement.pojo.Testtube;
 import com.higher.pcmanagement.pojo.bo.PageRequestBo;
 import com.higher.pcmanagement.pojo.bo.PageResultBo;
@@ -60,6 +59,7 @@ public class TestTubeServiceImpl implements TestTubeService {
 
     /**
      * 修改试管信息
+     *
      * @param testtube
      * @throws BusinessException
      */
@@ -82,15 +82,13 @@ public class TestTubeServiceImpl implements TestTubeService {
 //        获取输入框中编码的试管信息
         Testtube newtube = testTubeDao.getTubeByCode(testtube.getTesttubeCode());
         System.out.println(newtube);
-//        Integer testtubeId = newtube.getTesttubeId();
+
         if (newtube != null){
             Integer testtubeId = newtube.getTesttubeId();
             if (testtubeId!=testtubeId1){
-                throw new BusinessException("试管编码已存在，请重新输入", ResultCodeEnum.ERROR);
+               throw new BusinessException("试管编码已存在，请重新输入", ResultCodeEnum.ERROR);
             }
-
-        }else {
-
+        }
 //        人数条不能大于试管类型
         if (peopleCount<=collectType){
             if (tubeStatus==status){
@@ -116,7 +114,6 @@ public class TestTubeServiceImpl implements TestTubeService {
         }else {
             throw new BusinessException("试管中已采集"+peopleCount+"条信息，试管类型不能小于此数",ResultCodeEnum.ERROR);
         }
-        }
 
     }
 
@@ -128,3 +125,8 @@ public class TestTubeServiceImpl implements TestTubeService {
         testTubeDao.deleteSample(testtubeId);
     }
 }
+
+
+
+
+
